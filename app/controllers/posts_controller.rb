4f2cbee3cf14613f7ca109bfdb47e33e73a21ded
lauren-kohn <lauren.kohn@soft-adv.com>
2021-08-04 19:29:26 -0500

    commit tree c35ab9a55fcad2694e7eb9cd59800f18985fe50f

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
 
+  def new 
+    @post = Post.new(author_id: params[:author_id])
+  end
+  
   def index
     if params[:author_id]
       @posts = Author.find(params[:author_id]).posts
@@ -17,7 +21,11 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    if params[:author_id] && !Author.exists?(params[:author_id])
+      redirect_to authors_path, alert: "Author not found."
+    else
+      @post = Post.new(author_id: params[:author_id])
+    end
   end
 
   def create
@@ -33,12 +41,26 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+    if params[:author_id]
+      # establish existence of author_id
+      author = Author.find_by(id: params[:author_id])
+        # if it's there, find valid author
+      if author.nil?
+        redirect_to authors_path, alert: "Author not found."
+      else
+        # if an author is found, find posts using id
+        @post = author.posts.find_by(id: params[:id])
+        # filter through author.posts collection to make sure post id is in the author's posts
+        redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+      end
+    else
+      @post = Post.find(params[:id])
+    end
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :author_id)
   end
 end
